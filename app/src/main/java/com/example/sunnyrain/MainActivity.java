c4246ae2ca051ed5e65d07241d363454b69c5179
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2023-05-18 23:51:17 +0300

    added temperature api

--- app/src/main/java/com/example/sunnyrain/MainActivity.java
+++ app/src/main/java/com/example/sunnyrain/MainActivity.java
@@ -1,13 +1,114 @@
 package com.example.sunnyrain;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private String key = "731af0b97b4e4efd949193341231805";
+    String urlCurrentWeatherBase = "https://api.weatherapi.com/v1/current.json?key=";
+
+    private EditText userField;
+    private Button mainBtn;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userField = findViewById(R.id.user_field);
+        mainBtn = findViewById(R.id.search_btn);
+        result = findViewById(R.id.result);
+
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (userField.getText().toString().trim().equals("")) {
+                    Toast.makeText(MainActivity.this, R.string.emptyUserInput, Toast.LENGTH_LONG).show();
+                } else {
+                    String city = userField.getText().toString();
+                    String url = urlCurrentWeatherBase + key + "&q=" + city + "&aqi=no&lang=ru";
+
+                    new GetUrlData().execute(url);
+                }
+
+            }
+        });
+    }
+
+    private class GetUrlData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            result.setText("Waiting please");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                    return buffer.toString();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return null;
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String resultInfo){
+            super.onPostExecute(resultInfo);
+            try {
+                JSONObject obj = new JSONObject(resultInfo);
+                result.setText("Temperature:" + obj.getJSONObject("current").getDouble("temp_c"));
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
